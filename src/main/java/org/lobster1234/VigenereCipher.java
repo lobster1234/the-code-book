e609c07b7f5290b2e60b494c7e8e700e0baef806
lobster1234 <lobster1234@yahoo.com>
2022-12-22 23:54:47 -0800

    Added Vigenere Cipher implementation for encryption.

---
+++ src/main/java/org/lobster1234/VigenereCipher.java
@@ -0,0 +1,57 @@
+package org.lobster1234;
+
+/**
+ * The Vigenere Cipher - Page 48 of the book.
+ * While the book refers the Vigenere Square with Caesar shift of 1, most references online
+ * have no shift.
+ * In this implementation I've not used the "row 1 col 0 = shift by 1" model as it is called out in the book,
+ * but the code can be easily adjusted to shift if needed.
+ */
+public class VigenereCipher {
+    char[][] square = new char[26][26];
+
+    String keyPhrase;
+
+    public VigenereCipher(String keyPhrase){
+        this.keyPhrase = keyPhrase;
+        for(int rows = 0;rows <26;rows++){
+            for(int cols = 0;cols<26;cols++){
+                char toInsert = (char) ('a'+cols+rows);
+                if(toInsert > 'z') toInsert = (char) (toInsert -'z'+'a'-1);
+                square[rows][cols] = toInsert;
+            }
+        }
+    }
+
+    public String encrypt(String plainText){
+        //We use the key phrase to build a key stream the same size as plain text
+        StringBuilder builder = new StringBuilder();
+        StringBuilder keyStream = new StringBuilder();
+        while(keyStream.length() <= plainText.length()){
+            keyStream.append(keyPhrase);
+        }
+        keyStream.delete(plainText.length(),keyStream.length());
+        //now we build the cipher text
+        for(int i=0;i<plainText.length();i++){
+            //key stream defines the rows, plaintext defines the columns
+            int row = keyStream.charAt(i)-'a';
+            int col = plainText.charAt(i)-'a';
+            builder.append(square[row][col]);
+        }
+        return builder.toString();
+    }
+
+    public String decrypt(String cipherText){
+        //TODO implement this
+        return cipherText;
+    }
+
+    public void printSquare(){
+        for(int rows = 0;rows<26;rows++){
+            for(int cols = 0; cols < 26; cols++){
+                System.out.printf(" %c ", square[rows][cols]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/test/java/org/lobster1234/VigenereCipherTest.java
@@ -0,0 +1,23 @@
+package org.lobster1234;
+
+import junit.framework.TestCase;
+
+public class VigenereCipherTest extends TestCase {
+
+    VigenereCipher c;
+    public void setUp() {
+        c = new VigenereCipher("white");
+    }
+
+    public void testPrintSquare(){
+        c.printSquare();
+    }
+    public void testEncrypt() {
+        String cipherText = c.encrypt("diverttroopstoeastridge");
+        assertEquals(cipherText, "zpdxvpazhslzbhiwzbkmznm");
+    }
+
+    public void testDecrypt() {
+        //TODO implement this
+    }
+}
